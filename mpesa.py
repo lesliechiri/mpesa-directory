25cfce135f27aa07d3695805535d27e9e458f773
lesliechiri <dianawantes@gmail.com>
2019-04-30 06:55:25 +0300

    adding directory

--- mpesa.py
+++ mpesa.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class MpesaAccount:
     def __init__(self,name,number,balance):
         self.name = name
@@ -6,6 +8,8 @@ class MpesaAccount:
         self.deposits = []
         self.withdrawals = []
         self.loan = 0
+        self.loans = []
+
         
 
     def withdrawal_amount(self,x):
@@ -14,13 +18,19 @@ class MpesaAccount:
             print("cant withdraw")
         else:   
             self.balance = self.balance - x
-            self.withdrawals.append(x)
+            
+            now = datetime.now()
+            object = {"time":now,"x":x}
+            self.withdrawals.append(object)
             data = "Dear {} withdrawal of kes {} was successful current balance is{}".format(self.name,x,self.balance)
             print(data)
     def deposit(self,y):
         deposit = y
         self.balance = self.balance + y
-        self.deposits.append(y)
+        
+        now = datetime.now()
+        object = {"time":now,"y":y}
+        self.deposits.append(object)
         
         depo = "Dear {} deposit of kes {} was successful current balance is {}".format(self.name,y,self.balance)
         print(depo)
@@ -34,19 +44,35 @@ class MpesaAccount:
     def my_deposits(self):
         
         for x in self.deposits:
-            print(x)
+            time = x["time"].strftime("%A,%d,%B,%Y")
+            y = x["y"]
+            print("on {} you deposited  {}".format(time,y))
 
     def my_withdrawals(self):
 
-        for y in self.withdrawals:
-            print(y)
+        for a in self.withdrawals:
+
+            
+            time = a["time"].strftime("%A,%d,%B,%Y")
+            x = a["x"]
+            print("on {}  you withdrew {}".format(time,x))
+           
     def give_loan(self,g):
         loan = g 
-        
+        total = 0
+        for x in self.deposits:
+                y = x["y"]
+                total+=y
         
 
-        if len(self.deposits)>=5 and g<=(1/3*sum(self.deposits)) and self.loan==0:
+        if len(self.deposits)>=5 and g<=total/3 and self.loan==0:
             self.loan = self.loan + g
+            
+            
+
+            now = datetime.now()
+            object = {"time":now,"g":g}
+            self.loans.append(object)
             print("Dear customer your loan of {} has been approved".format(g))
         else:
             print("loan not approved") 
@@ -77,12 +103,44 @@ class MpesaAccount:
             self.balance = excess_cash+self.balance
         elif a == self.loan:
             self.loan = self.loan-a
-            print("Dear customer your loan is now fully paid")    
-       
+            print("Dear customer your loan is now fully paid")
+
+
+    def my_loans(self):
+
+
+     
+        for b in self.loans:
+
             
-      
+            time = b["time"].strftime("%A,%d,%B,%Y")
+            b = a["g"]
+            print("on {}  you recieved a loan of {}".format(time,g))
+
+    def statements(self):
        
-           
+
+        for x in self.deposits:
+            time = x["time"].strftime("%A,%d,%B,%Y")
+            y = x["y"]
+            print("on {} you deposited  {}".format(time,y))
+
+
+        for a in self.withdrawals:
+
+            
+            time = a["time"].strftime("%A,%d,%B,%Y")
+            x = a["x"]
+            print("on {}  you withdrew {}".format(time,x))
+
+
+        for b in self.loans:
+
+            
+            time = b["time"].strftime("%A,%d,%B,%Y")
+            g = b["g"]
+            print("on {}  you recieved a loan of {}".format(time,g))        
+
         
 
         
